b8e7aa8d2f63565638b1a4b43d4b8062f043949f
Harry Morgan <harrymorgan@ip-192-168-0-86.eu-west-2.compute.internal>
2021-11-03 14:32:43 +0000

    add cookie auth bypass

--- main.py
+++ main.py
@@ -12,12 +12,25 @@ driver = webdriver.Chrome('./chromedriver')
 
 # Scrape individual items
 def getProductNames():
+    products = []
     soup = BeautifulSoup(page.content, "html.parser")
     items = soup.select('a[data-testid="product__item"]')
     for item in items:
-        print(item['href'][9:])
-    return items
+        products.append(item['href'][9:])
+    return products
 
 productPages = getProductNames()
-driver.get(depopURL + 'edit' + productPages[0])
 
+def getLoginCookies():
+    with open("cookies.data", "r") as a_file:
+        driver.get(depopURL + '/products/deit' + productPages[1])
+        for line in a_file:
+            stripped_line = line.strip()
+            cookie = stripped_line.split(" ", 1)
+            print("cookies in line:")
+            print("name: ", cookie[0], "val: ", cookie[1])
+            driver.add_cookie({"name" : cookie[0], "value" : cookie[1], "path" : "/", "domain": ".depop.com"})
+
+getLoginCookies()
+driver.get(depopURL + '/products/edit' + productPages[0])
+driver.page_source.encode("utf-8")

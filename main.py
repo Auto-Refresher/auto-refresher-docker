3593d7b3011d4f47b3aca3aa9557d4066c21c31f
Harry Morgan <harrymorgan@Harrys-MacBook-Air.local>
2021-11-03 12:05:45 +0000

    Retrieve product hrefs list, add chromdriver

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 
-URL = "https://www.depop.com/harrywmo/"
-page = requests.get(URL)
+depopURL = "https://www.depop.com"
+user = "/harrywmo"
+page = requests.get(depopURL + user)
+
+driver = webdriver.Chrome('./chromedriver')
 
 # Scrape individual items
-soup = BeautifulSoup(page.content, "html.parser")
-items = soup.select('a[data-testid="product__item"]')
-for item in items:
-    print(item)
+def getProductNames():
+    soup = BeautifulSoup(page.content, "html.parser")
+    items = soup.select('a[data-testid="product__item"]')
+    for item in items:
+        print(item['href'][9:])
+    return items
 
-
+productPages = getProductNames()
+driver.get(depopURL + 'edit' + productPages[0])
 
